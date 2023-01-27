3e1eec1957f4f5ff0ce92b61eb29f1ddc1883f61
daybreak53 <cjdcns1214@ddfactory.kr>
2023-01-27 14:53:37 +0900

    [CU] 1004 / Java / 정답

---
+++ CJY/CU/No_1004.java
@@ -0,0 +1,14 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class No_1004 {
+    public static void main (String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("'Hello'");
+
+        bw.flush();
+        bw.close();
+    }
+}
