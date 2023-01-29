9acc218150fff0f361bc63e823913ba6fd288bed
Daybreak53 <wnghddk123@naver.com>
2023-01-29 19:34:51 +0900

    [CU] 1055 / Java / 정답

---
+++ CJY/CU/No_1055.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class No_1055 {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
+
+        br.close();
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(arr[0].equals("1") || arr[1].equals("1") ? "1" : "0");
+
+        bw.flush();
+        bw.close();
+    }
+}
