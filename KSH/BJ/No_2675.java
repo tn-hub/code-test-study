73fd02eefea37581bd4d848b9c42a307cd4ef9a7
suhyeon.k <rantatn8@gmail.com>
2023-02-09 22:26:37 +0900

    [BJ] 2675 / Java / 정답

--- KSH/BJ/No_2675.java
+++ KSH/BJ/No_2675.java
@@ -1,6 +1,6 @@
 import java.io.*;
 
-public class No_42675 {
+public class No_2675 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
